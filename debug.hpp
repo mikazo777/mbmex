51f8720ab176e619059801a9f1bc35b140e2d170
Norimasa Mikami <mikazo777@gmail.com>
2020-01-02 00:28:48 +0900

    Change the define val

--- debug.hpp
+++ debug.hpp
@@ -1,9 +1,9 @@
-#ifndef __DEBUG_HPP__
-#define __DEBUG_HPP_
+#ifndef DEBUG_HPP
+#define DEBUG_HPP
 #include <iostream>
 
 //#define __MMCI_DEBUG_ON__
 using namespace std;
 void debugPrint(string str);
 void tracePrint(string str);
-#endif // __DEBUG_HPP__
+#endif // DEBUG_HPP
