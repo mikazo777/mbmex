36261f410902623588e59b1f07998a8d59bb2133
Norimasa Mikami <mikazo777@gmail.com>
2020-01-05 03:16:45 +0900

    Add to measures the double include

--- logitechMouseProduct.hpp
+++ logitechMouseProduct.hpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#ifndef LOGITECH_MOUSE_PRODUCT_HPP
+#define LOGITECH_MOUSE_PRODUCT_HPP
 const int LOGITECH_MOUSE = 0x046d;
 const int MX_REVOLUTION_1 = 0xc51a;
 const int MX_REVOLUTION_2 = 0xc525;
@@ -7,3 +9,4 @@ const int MX_REVOLUTION_4 = 0xc52b;
 const int MX_REVOLUTION_5 = 0xb007;
 const int MX_5500 = 0xc71c;
 const int MX_MASTER2S_KEYBOARD = 0xb019;
+#endif // LOGITECH_MOUSE_PRODUCT_HPP
