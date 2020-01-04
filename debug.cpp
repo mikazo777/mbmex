4178f65cd9965c1507ee7dcfbc30dba21e4ec204
Norimasa Mikami <mikazo777@gmail.com>
2020-01-04 16:38:35 +0900

    Change Enable Debug define

--- debug.cpp
+++ debug.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 void debugPrint(string str) {
-#ifdef MMCI_DEBUG_ON
+#ifdef MBMEX_DEBUG_ON
 	cout << "error no = " << errno << endl;
 	
 	perror(str.c_str());
 	return;
-#endif // MMCI_DEBUG_ON
+#endif // MBMEX_DEBUG_ON
 }
 
 void tracePrint(string str) {
-#ifdef MMCI_DEBUG_ON
+#ifdef MBMEX_DEBUG_ON
 	cout << str << endl;
-#endif // MMCI_DEBUG_ON
+#endif // MBMEX_DEBUG_ON
 }
