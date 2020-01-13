eaa78d9eb7877b4f9972382a654114c9210aeed1
Norimasa Mikami <mikazo777@gmail.com>
2020-01-13 12:25:29 +0900

    Add to print file line function

--- debug.cpp
+++ debug.cpp
@@ -1,9 +1,20 @@
 #include "debug.hpp"
+#include <cstdio>
 using namespace std;
 
+void msgPrint(const char *pPrintMsg,
+			  const char *pFile,
+			  const int pLine,
+			  const char *pFunction) {
+#ifdef MBMEX_DEBUG_ON
+	cout << pFile << ":" << dec << pLine << ":" << pFunction << ":" << pPrintMsg <<  endl;
+#endif
+	return;
+}
 void debugPrint(string str) {
 #ifdef MBMEX_DEBUG_ON
-	cout << "error no = " << errno << endl;
+//	cout << "error no = " << errno << endl;
+	//mbmexDebug::msgPrint(__builtin_FILE(), __builtin_LINE());
 	
 	perror(str.c_str());
 	return;
--- debug.hpp
+++ debug.hpp
@@ -1,9 +1,15 @@
 #ifndef DEBUG_HPP
 #define DEBUG_HPP
 #include <iostream>
+#include <cstdio>
+#include <stdio.h>
 
 //#define MBMEX_DEBUG_ON
 using namespace std;
 void debugPrint(string str);
 void tracePrint(string str);
+void msgPrint(const char *pPrintMsg,
+			  const char *pFile = __builtin_FILE(),
+			  int lineNum = __builtin_LINE(),
+			  const char *pFunc = __builtin_FUNCTION());
 #endif // DEBUG_HPP
