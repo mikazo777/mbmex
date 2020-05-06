0a6243749cdc4d1336da21015eb0e19add37656f
Norimasa Mikami <mikazo777@gmail.com>
2020-05-06 22:06:54 +0900

    Delete the unnecessary files

--- main.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef MBMEX_MAIN_HPP
-#define MBMEX_MAIN_HPP
-
-#endif	// MBMEX_MAIN_HPP
--- mbmexBase.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef MBMEX_BASE_HPP
-#define MBMEX_BASE_HPP
-#include <dirent.h>
-const char devInputDir[] = "/dev/input";
-int checkEventDevice(const struct dirent *pDir);
-#endif	// MBMEX_MAIN_HPP
